5f54338d8117c1fe8e602148f7ea889170ad77ca
AnwarAlishba <anwarhussainyahoo@gamail.com>
2022-11-28 16:54:27 +0530

    Chenging the backgroung

--- app/src/main/java/com/growthpanda/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/growthpanda/myapplicationtest/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 count=count+1;
                 textView.setText("You have pressed : "+count+" times");
                 Toast.makeText(MainActivity.this, "Count = "+count, Toast.LENGTH_SHORT).show();
