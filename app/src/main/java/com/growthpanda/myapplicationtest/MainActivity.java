d757aa068a94397a3c00045b1561080a7f1b15ad
AnwarAlishba <anwarhussainyahoo@gamail.com>
2022-11-28 16:17:52 +0530

    Testing Purpose

--- app/src/main/java/com/growthpanda/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/growthpanda/myapplicationtest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
                 count=count+1;
                 textView.setText("You have pressed : "+count+" times");
+                Toast.makeText(MainActivity.this, "Count = "+count, Toast.LENGTH_SHORT).show();
 
             }
         });
