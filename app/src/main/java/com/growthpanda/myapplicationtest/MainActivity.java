2de8e4af8debdd425d39e26f77dcb26bc16fb25c
AnwarAlishba <anwarhussainyahoo@gamail.com>
2022-11-28 16:11:24 +0530

    Testing Purpose

--- app/src/main/java/com/growthpanda/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/growthpanda/myapplicationtest/MainActivity.java
@@ -3,12 +3,30 @@ package com.growthpanda.myapplicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int count=0;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                count=count+1;
+                textView.setText("You have pressed : "+count+" times");
+
+            }
+        });
     }
 }
